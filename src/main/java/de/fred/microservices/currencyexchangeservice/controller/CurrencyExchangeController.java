dc1f1d0197c2fb83aa2047fd75408d1aa33270a9
Fred <frederickklyk@gmail.com>
2024-02-19 19:16:54 +0100

    expose port as response

--- src/main/java/de/fred/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/de/fred/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package de.fred.microservices.currencyexchangeservice.controller;
 
 import de.fred.microservices.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,21 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to
     ) {
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
