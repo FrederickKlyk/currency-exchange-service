5cc5f911e5e017844d08df6e593dd788f423d332
Fred <frederickklyk@gmail.com>
2024-02-22 12:56:05 +0100

    add jpa integration, h2 and initial data.sql queries

--- src/main/java/de/fred/microservices/currencyexchangeservice/model/CurrencyExchange.java
+++ src/main/java/de/fred/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,10 +1,18 @@
 package de.fred.microservices.currencyexchangeservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+    @Id
     private long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
