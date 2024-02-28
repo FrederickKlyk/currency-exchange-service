d9519fc15d76e273a5b2b267837d271ae25536ea
Frederick Klyk <klyk@adesso-mobile.de>
2024-02-28 15:46:18 +0100

    resilience4j properties for different cases for an api

--- src/main/java/de/fred/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/de/fred/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,11 @@
 package de.fred.microservices.currencyexchangeservice.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
@@ -16,11 +14,17 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    //@CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //RateLimiter: z.B. 10s => 10000 calls to the sample api, man kan verschiedne RateLimits für die jeweiligen API Methoden (siehe name="") in diesem Microservice festlegen
+    //@RateLimiter(name = "sample-api")
+    @Bulkhead(name = "sample-api")
     public String sampleApi() {
         logger.info("Sample API Call received.");
+        /*
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
+        */
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception e) {
