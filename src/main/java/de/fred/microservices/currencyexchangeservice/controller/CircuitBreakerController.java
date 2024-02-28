5d2b7a0525b6ee5aeef9257b99c6501bb2f01bec
Frederick Klyk <klyk@adesso-mobile.de>
2024-02-28 12:26:31 +0100

    Resilience4j Circuit Breaker

--- src/main/java/de/fred/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/de/fred/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,29 @@
 package de.fred.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
-    public String sampleApi(){
-        return "Sample API";
+    //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample API Call received.");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception e) {
+        return "fallback-response";
     }
 }
